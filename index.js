cf6a41d477fdafa1c91709151db3ef62410b7b4c
“Calixto” <l19070031@huetamo.tecnm.mx>
2024-09-20 13:00:42 -0600

    configuración index

---
+++ index.js
@@ -0,0 +1,25 @@
+//server index.js
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+require('./server/database/database.js');
+
+app.use(cors());
+app.use(express.json());
+app.disable('x-powered-by');
+
+
+app.use('/api/users',require('./server/routes/users.js'));
+
+
+app.get('/api', async (req, res) => {
+    res.json({ message: 'Hello from server!' })
+});
+
+
+
+module.exports =  app;
+
